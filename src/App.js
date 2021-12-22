0fa089b14ae3e0bc96b7f6fa3c863baf6b2521a7
RexhinaQevani <reginaqevani@gmail.com>
2021-12-22 15:49:47 +0100

    Added footer with github url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://rexhinaqevani.netlify.app/index.html" target="_blank">
+          Rexhina Qevani
+        </a>{" "}
+        and it is{" "}
+        <a
+          href="
+          https://github.com/RexhinaQevani/my-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
