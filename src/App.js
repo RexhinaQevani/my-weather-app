37212a28eace093e19f005d7be1a65eb54a5702e
RexhinaQevani <reginaqevani@gmail.com>
2021-12-22 15:55:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://rexhinaqevani.netlify.app/index.html" target="_blank">
+        <a
+          href="https://rexhinaqevani.netlify.app/index.html"
+          target="_blank"
+          rel="noreferrer"
+        >
           Rexhina Qevani
         </a>{" "}
         and it is{" "}
@@ -15,6 +19,7 @@ export default function App() {
           href="
           https://github.com/RexhinaQevani/my-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
