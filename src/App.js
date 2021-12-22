ac101bd7c1d95379b306063503a9f27f23d7cbb6
RexhinaQevani <reginaqevani@gmail.com>
2021-12-22 17:43:37 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://rexhinaqevani.netlify.app/index.html"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rexhina Qevani
-        </a>{" "}
-        and it is{" "}
-        <a
-          href="
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://rexhinaqevani.netlify.app/index.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rexhina Qevani
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="
           https://github.com/RexhinaQevani/my-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
